0d495a48529beb1fb91f248874707472bb0ff7fa
Surinna Curtis <ekiru.0@gmail.com>
2017-10-08 16:29:45 +0000

    Add docs for the views package.

---
+++ views/doc.go
@@ -0,0 +1,4 @@
+// The views package defines helper functions and types for defining
+// request handlers in Kanna in order to centralize such logic as
+// loading templates, handling content-types, etc.
+package views
--- views/html.go
+++ views/html.go
@@ -10,6 +10,7 @@ type htmlView struct {
 	content string
 }
 
+// Html views serve the passed string as an HTML document.
 func Html(doc string) http.Handler {
 	return &htmlView{
 		content: doc,
@@ -25,16 +26,23 @@ func (view *htmlView) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	sendHtml(w, []byte(view.content))
 }
 
+// HtmlTemplate views serve an HTML document from a template defined
+// using the template/html package.
 type HtmlTemplate struct {
 	template *template.Template
 }
 
+// ParseHtmlTemplate parses the source string as a template and
+// constructs a HtmlTemplate view for the template. If an error occurs
+// parsing the template, ParseHtmlTemplate will panic.
 func ParseHtmlTemplate(source string) *HtmlTemplate {
 	return &HtmlTemplate{
 		template: template.Must(template.New("").Parse(source)),
 	}
 }
 
+// Render renders the template to the response writer, passing the
+// supplied data to the template.
 func (template *HtmlTemplate) Render(w http.ResponseWriter, r *http.Request, data interface{}) {
 	var output bytes.Buffer
 	if err := template.template.Execute(&output, data); err != nil {
